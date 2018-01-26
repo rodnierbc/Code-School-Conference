bb4c117618598367e1e3708957a1cb3247fdb420
Rodnier Borrego <rodnierbc89@gmail.com>
2018-01-26 15:46:15 -0800

    Create the Data Base

--- src/main/java/models/Conference.java
+++ src/main/java/models/Conference.java
@@ -3,21 +3,32 @@ import java.util.Date;
 
 public class Conference {
     private int id;
-    private Date date;
+    private Date confDate;
     private String topic;
     private String address;
     private int instructorId;
+    private int sponsorId;
     private int attendeesAverageAge;
     private double attendeesAverageSalary;
     private int womenNumber;
     private int menNumber;
 
-    public Conference(Date date, String topic, String address, int instructorId){
-        this.date = date;
+    public int getSponsorId() {
+        return sponsorId;
+    }
+
+    public void setSponsorId(int sponsorId) {
+        this.sponsorId = sponsorId;
+    }
+
+    public Conference(Date confDate, String topic, String address, int instructorId, int sponsorId){
+        this.confDate = confDate;
         this.topic = topic;
         this.address = address;
         this.instructorId = instructorId;
+        this.sponsorId = sponsorId;
         this.attendeesAverageAge = 0;
+
         this.attendeesAverageSalary = 0;
         this.womenNumber = 0;
         this.menNumber = 0;
@@ -31,12 +42,12 @@ public class Conference {
         this.topic = topic;
     }
 
-    public Date getDate() {
-        return date;
+    public Date getConfDate() {
+        return confDate;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setConfDate(Date confDate) {
+        this.confDate = confDate;
     }
 
     public int getId() {
@@ -91,18 +102,20 @@ public class Conference {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Conference conference = (Conference) o;
-        if (!date.equals(conference.date)) return false;
+        if (!confDate.equals(conference.confDate)) return false;
         if (!topic.equals(conference.topic)) return false;
         if (instructorId != conference.instructorId) return false;
+        if (sponsorId != conference.sponsorId) return false;
         return address != null ? address.equals(conference.address) : conference.address == null;
     }
 
     @Override
     public int hashCode() {
-        int result = date.hashCode();
+        int result = confDate.hashCode();
         result = 31 * result + topic.hashCode();
         result = 31 * result + address.hashCode();
         result = 31 * result + instructorId;
+        result = 31 * result + sponsorId;
         return result;
     }
 }
---
+++ src/main/resources/db/conference.sql
@@ -0,0 +1,44 @@
+SET MODE PostgreSQL;
+
+CREATE TABLE IF NOT EXISTS conferences (
+ id int PRIMARY KEY auto_increment,
+ topic VARCHAR,
+ confDate VARCHAR,
+ address VARCHAR,
+ instructorId INTEGER,
+ sponsorId INTEGER,
+ attendeesAverageAge FLOAT,
+ attendeesAverageSalary FLOAT,
+ womenNumber INTEGER,
+ menNumber INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS sponsors (
+ id int PRIMARY KEY auto_increment,
+ companyName VARCHAR
+ website VARCHAR
+ representativeName VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS instructor (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ email VARCHAR,
+ phone VARCHAR,
+ education VARCHAR,
+ currentJob VARCHAR,
+);
+CREATE TABLE IF NOT EXISTS attendees (
+ id int PRIMARY KEY auto_increment,
+ name VARCHAR,
+ email VARCHAR,
+ phone VARCHAR,
+ salary FLOAT,
+ birthdate VARCHAR,
+ gender VARCHAR,
+);
+CREATE TABLE IF NOT EXISTS attendees_conferences (
+ id int PRIMARY KEY auto_increment,
+ conferenceId INTEGER,
+ attendeId INTEGER
+);
